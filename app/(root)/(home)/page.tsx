c638bcc00e8545ffefe14581a06416cc41486a5f
Avocoder <andrii.otroshenko@gmail.com>
2024-05-24 15:06:32 +0300

    ADD: home page features, question card, filter, some utils function

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,6 +5,77 @@ import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 import React from "react";
+import QuestionCard from "@/components/card/QuestionCard";
+import NoResult from "@/components/shared/NoResult";
+
+const QuestionCardItems = [
+  {
+    _id: 1,
+    title:
+      "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
+    tags: [
+      {
+        _id: 1,
+        name: "Next.JS",
+      },
+      {
+        _id: 2,
+        name: "React",
+      },
+    ],
+    views: 14020,
+    answers: [],
+    upvotes: 1000,
+    createdAt: new Date("2024-01-04"),
+    author: {
+      _id: 1,
+      name: "Andrii Otroshenko",
+      picture: "/assets/images/default-logo.svg",
+    },
+  },
+  {
+    _id: 2,
+    title: "Redux Toolkit Not Updating State as Expected",
+    tags: [
+      {
+        _id: 3,
+        name: "Redux",
+      },
+      {
+        _id: 2,
+        name: "React",
+      },
+    ],
+    author: {
+      _id: 1,
+      name: "Andrii Otroshenko",
+      picture: "/assets/images/default-logo.svg",
+    },
+    views: 1567892,
+    answers: [], // Adjusted the structure to match Array<Object>
+    upvotes: 41982,
+    createdAt: new Date("2024-02-07"),
+  },
+  {
+    _id: 3,
+    title: "Async/Await Function Not Handling Errors Properly",
+    tags: [
+      {
+        _id: 4,
+        name: "Fetch",
+      },
+    ],
+    author: {
+      _id: 1,
+      name: "Andrii Otroshenko",
+      picture: "/assets/images/default-logo.svg",
+    },
+    views: 278,
+    answers: [], // Adjusted the structure to match Array<Object>
+    upvotes: 24,
+    createdAt: new Date("2024-03-24"),
+  },
+];
 
 const Home = () => {
   return (
@@ -38,6 +109,43 @@ const Home = () => {
         />
       </div>
       <HomeFilters />
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {QuestionCardItems.length > 0 ? (
+          QuestionCardItems.map(
+            ({
+              _id,
+              title,
+              tags,
+              author,
+              upvotes,
+              views,
+              answers,
+              createdAt,
+            }) => (
+              <QuestionCard
+                key={_id}
+                _id={_id}
+                title={title}
+                tags={tags}
+                author={author}
+                upvotes={upvotes}
+                views={views}
+                answers={answers}
+                createdAt={createdAt}
+              />
+            )
+          )
+        ) : (
+          <NoResult
+            title="There's no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and
+        kickstart the discussion. our query could be the next big
+        thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 };
---
+++ components/card/QuestionCard.tsx
@@ -0,0 +1,93 @@
+import Link from "next/link";
+import React from "react";
+import Tag from "../ui/tag";
+import { formatAndDivideNumber, getTimeStamp } from "@/lib/utils";
+import Metric from "../shared/Metric";
+
+type QuestionCardProps = {
+  _id: number;
+  title: string;
+  createdAt: Date;
+  tags: { _id: number; name: string }[];
+  author: {
+    _id: number;
+    name: string;
+    picture: string;
+  };
+
+  views: number;
+  answers: Array<Object>;
+  upvotes: number;
+};
+
+const QuestionCard = ({
+  _id,
+  title,
+  createdAt,
+  tags = [],
+  author,
+  views,
+  answers,
+  upvotes,
+}: QuestionCardProps) => {
+  return (
+    <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
+      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
+        <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
+          {getTimeStamp(createdAt)}
+        </span>
+        <Link href={`/question/${_id}`}>
+          <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
+            {title}
+          </h3>
+        </Link>
+      </div>
+      {/* If signed in add edit delete actions */}
+      <div className="mt-3.5 flex flex-wrap gap-2">
+        {tags.map(({ _id, name }) => (
+          <Link
+            key={_id}
+            className="flex justify-between gap-2"
+            href={`/tags/${_id}`}
+          >
+            <Tag>{name}</Tag>
+          </Link>
+        ))}
+      </div>
+      <div className="flex-between mt-6 w-full flex-wrap gap-3 ">
+        <Metric
+          imgUrl={author?.picture}
+          alt="User"
+          value={author.name}
+          title={` • asked ${getTimeStamp(createdAt)}`}
+          href={`/profile/${author._id}`}
+          isAuthor
+          textStyles="body-medium text-dark400_light700"
+        />
+        <Metric
+          imgUrl="/assets/icons/like.svg"
+          alt="Upvotes"
+          value={formatAndDivideNumber(upvotes)}
+          title=" Votes"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/message.svg"
+          alt="Message"
+          value={formatAndDivideNumber(answers.length)}
+          title=" Answers"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/eye.svg"
+          alt="Eye"
+          value={formatAndDivideNumber(views)}
+          title=" Views"
+          textStyles="small-medium text-dark400_light800"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
